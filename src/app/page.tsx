d7f0891f518c8eb53e39e15cac2fe38de376e50f
Ralph Arvin De Castro <ralpharvin.dc@gmail.com>
2023-05-05 16:20:59 -0400

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import ClimateRiskRatingTable from './components/ClimateRiskRatingTable';
-import { usePapaParse } from 'react-papaparse';
 import ClimateRiskRatingMap from './components/ClimateRiskRatingMap';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -10,7 +8,6 @@ export default async function Home() {
   return (
     <>
       <ClimateRiskRatingMap />
-      {/* <ClimateRiskRatingTable /> */}
     </>
   );
 }
